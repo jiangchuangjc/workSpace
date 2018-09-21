0bb26cc0ee2ed451dd508a30fc3c8b2ce38b88cd
JiangChuang <2952830597@qq.com>
2018-09-21 13:10:31 +0800

    第五天

--- 2048game/js/2048.js
+++ 2048game/js/2048.js
@@ -13,18 +13,39 @@ var game={
     }
     this.randomnum();
     this.randomnum();
+    this.updateView();
     console.log(this.data.join("\n"));
     
   },
-  randomnum(){
+  randomnum:function(){
      while(true){
-        r=Math.floor(Math.random()*this.RN);
-        c=Math.floor(Math.random()*this.CN);
+        var r=Math.floor(Math.random()*this.RN);
+        var c=Math.floor(Math.random()*this.CN);
        if(this.data[r][c]==0){
          this.data[r][c]=Math.random()<0.5?2:4;
          break;
        }
      }
-  }
+  },
+  updateView:function(){
+		//将data中的每个元素值更新到页面对应的div上
+		for(var r=0;r<this.RN;r++){//遍历data
+			for(var c=0;c<this.CN;c++){
+				//找到和r行c列位置对应的div
+				var div=document.getElementById("c"+r+c);
+				//如果data中当前元素的值不等于0
+				if(this.data[r][c]!==0){
+					//设置div的内容为data中当前元素的值
+					div.innerHTML=this.data[r][c];
+					//设置div的class为"cell n"+当前元素值
+					div.className="cell n"+this.data[r][c];
+				}else{//否则
+					div.innerHTML="0";//清除div的内容
+					//设置div的class为"cell"
+					div.className="cell";
+				}
+			}
+		}
+}
 }
 game.start();
