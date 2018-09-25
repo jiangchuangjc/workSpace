8814e20d2119597a7097276b1fce44d92e04e25f
JiangChuang <2952830597@qq.com>
2018-09-25 19:52:02 +0800

    第六天

---
+++ day06/data3.php
@@ -0,0 +1,8 @@
+<?php
+//获取参数
+  $param = $_POST["sname"];
+  $param1 = $_POST["age"];
+  // 返回数据
+  echo $param; 
+  echo $param1;
+?>
---
+++ day06/stu.php
@@ -0,0 +1,5 @@
+<?php
+  //echo file_get_contents("./student.json");
+  //echo $_GET["name"];
+  var_dump($_POST);
+?>
