36a8c4538db588bbdee913ff85798f3a91f9996d
JiangChuang <2952830597@qq.com>
2018-09-26 14:23:11 +0800

    第七天

--- day06/stu.php
+++ day06/stu.php
@@ -1,5 +1,5 @@
 <?php
-  //echo file_get_contents("./student.json");
-  //echo $_GET["name"];
-  var_dump($_POST);
+  echo file_get_contents("./student.json");
+  // //echo $_GET["name"];
+  // var_dump($_POST);
 ?>
